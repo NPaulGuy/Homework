1b724aaf88900be4b7b7644993ab7c1eb0242f94
PaulGuy <nikitapolgui@mail.ru>
2022-03-02 14:53:13 +0300

    Lesson-2: updated tasks

--- Lesson2/Task11.php
+++ Lesson2/Task11.php
@@ -7,7 +7,7 @@
  *  ];
  */
 $arr = [
-	'cms'=>['joomla', 'wordpress', 'drupal'],
-	'colors'=>['blue'=>'голубой', 'red'=>'красный', 'green'=>'зеленый']
+	'cms' => ['joomla', 'wordpress', 'drupal'],
+	'colors' => ['blue' => 'голубой', 'red' => 'красный', 'green' => 'зеленый']
 ];
 echo $arr['cms'][0] . $arr['cms'][2] . $arr['colors']['green'] . $arr['colors']['red'];
