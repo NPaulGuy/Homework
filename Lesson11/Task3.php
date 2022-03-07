4b75696167188a3b3dcbd609b9295cfd58b68a90
NPaulGuy <nikitapolgui@mail.ru>
2022-03-07 09:41:02 +0300

    Lesson-11: added tasks

---
+++ Lesson11/Task3.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * 	Сделайте функцию, которая отнимает от первого числа второе и делит на третье.
+ */
+function minusAndDivide($num, $minusNum, $divNum) {
+    return ($num - $minusNum) / $divNum;
+}
+echo minusAndDivide(5, 3, -2);
