5c463e54f4d7a0cb7ea48d353d7721226caf145f
NPaulGuy <nikitapolgui@mail.ru>
2022-03-30 15:38:56 +0300

    OOP_Lesson2: added PHPDoc

--- OOP_Lesson2/Task1.php
+++ OOP_Lesson2/Task1.php
@@ -2,23 +2,41 @@
 /**
  * Реализовать класс User с методом show() : метод принимает параметром строку и добавляет в конец "!!!".
  */
+/**
+ * [Description User]
+ */
 class User
 {
     private string $name;
     private int $age;
+    /**
+     * @param string $name
+     * @param int $age
+     */
     public function __construct(string $name, int $age) 
     {
         $this->name = $name;
         $this->age = $age;
     }
+    /**
+     * @return string
+     */
     public function getName() : string 
     {
         return $this->name;
     }
+    /**
+     * @return int
+     */
     public function getAge() : int 
     {
         return $this->age;
     }
+    /**
+     * @param string $str
+     * 
+     * @return string
+     */
     public function show(string $str) : string
     {
         return $str . "!!!";
