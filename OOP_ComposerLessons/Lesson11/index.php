d4e723521cc33fa91791848fcb4ac656e6f3c594
NPaulGuy <nikitapolgui@mail.ru>
2022-04-01 11:13:59 +0300

    OOP_Lesson11: added tasks

---
+++ OOP_ComposerLessons/Lesson11/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Создайте класс Student с полями name и course(курс обучения).
+ * Добавьте конструктор(поле course устанавливается в 1), геттеры-сеттеры, метод transferToNextCourse(переводит студента на следующий курс).
+ * Модифицируйте метод transferToNextCourse так, чтобы при переводе на новый курс выполнялась проверка того, что новый курс не будет больше 5.
+ */
+require __DIR__ . '/../vendor/autoload.php'; 
+use \Lesson11\Student;
+
+$student = new Student("Kirill");
+echo $student->getCourse() . "<br>";
+echo $student->transferToNextCourse()->getCourse();
---
+++ OOP_ComposerLessons/Lesson11/src/Student.php
@@ -0,0 +1,59 @@
+<?php
+namespace Lesson11;
+/**
+ * [Description Student]
+ */
+class Student
+{
+    private string $name;
+    private int $course;
+    /**
+     * @param string $name
+     * @param int $course
+     */
+    public function __construct(string $name) 
+    {
+        
+        $this->name = $name;
+        $this->course = 1;
+       
+    }
+    /**
+     * @return string
+     */
+    public function getName() : string 
+    {
+        return $this->name;
+    }
+    /**
+     * @return int
+     */
+    public function getCourse() : int 
+    {
+        return $this->course;
+    }
+    /**
+     * @return Student
+     */
+    public function transferToNextCourse() : Student
+    {
+        $newCourse = $this->course + 1;
+        if ($this->isCourseCorrect($newCourse)) {
+            $this->course = $newCourse;
+        }
+        return $this;
+    }
+    /**
+     * @param int $course
+     * 
+     * @return bool
+     */
+    private function isCourseCorrect(int $course) : bool 
+    {
+        if (isset($course)) {
+            return ($course <= 5);
+        } else {
+            return ($this->course <= 5);
+        }
+    }
+}
