03ad1a3e70d63d7b87eb82a871153f1c84873695
NPaulGuy <nikitapolgui@mail.ru>
2022-04-04 09:23:53 +0300

    OOP_Lesson12: added PHPDoc, updated code style

--- OOP_ComposerLessons/Lesson12/src/Arr.php
+++ OOP_ComposerLessons/Lesson12/src/Arr.php
@@ -1,21 +1,35 @@
 <?php
 namespace Lesson12;
+/**
+ * [Description Arr]
+ */
 class Arr
 {
 	private $numbers = [];
+	/**
+	 * @param array $arr
+	 * 
+	 * @return Arr
+	 */
 	public function add(array $arr) : Arr
 	{
 		foreach ($arr as $num) {
-            $this->numbers[] = $num;
-        }
-        return $this;
+			$this->numbers[] = $num;
+		}
+		return $this;
 	}
+	/**
+	 * @return int
+	 */
 	public function getSum() : int
 	{
 		return array_sum($this->numbers);
 	}
-    public function getAvg() : float
-    {
-        return array_sum($this->numbers) / count($this->numbers);
-    }
+    /**
+     * @return float
+     */
+	public function getAvg() : float
+	{
+		return array_sum($this->numbers) / count($this->numbers);
+	}
 }	
