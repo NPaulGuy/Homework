0f0fd83456e4cbeaa4728864ab1c75ed0e7547b3
NPaulGuy <nikitapolgui@mail.ru>
2022-04-04 10:08:53 +0300

    OOP_Lesson16: added tasks

--- OOP_ComposerLessons/vendor/composer/autoload_psr4.php
+++ OOP_ComposerLessons/vendor/composer/autoload_psr4.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Lesson16\\' => array($baseDir . '/Lesson16/src'),
     'Lesson15\\' => array($baseDir . '/Lesson15/src'),
     'Lesson14\\' => array($baseDir . '/Lesson14/src'),
     'Lesson13\\' => array($baseDir . '/Lesson13/src'),
