8d056bb3ec3146f8221af7a4023d4b6eff31b190
NPaulGuy <nikitapolgui@mail.ru>
2022-04-05 14:26:26 +0300

    OOP_Lesson26: added PHPDoc

--- OOP_ComposerLessons/Lesson26/src/iUser.php
+++ OOP_ComposerLessons/Lesson26/src/iUser.php
@@ -1,9 +1,28 @@
 <?php
 namespace Lesson26;
+/**
+ * [Description iUser]
+ */
 interface iUser
 {
+	/**
+	 * @param string $name
+	 * 
+	 * @return iUser
+	 */
 	public function setName(string $name) : iUser;
+	/**
+	 * @return string
+	 */
 	public function getName() : string;
+	/**
+	 * @param int $age
+	 * 
+	 * @return iUser
+	 */
 	public function setAge(int $age) : iUser;
+	/**
+	 * @return int
+	 */
 	public function getAge() : int;
 }
