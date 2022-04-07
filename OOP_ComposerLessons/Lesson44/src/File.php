cf8abf978d61c1b7f17490cd2099469e853e3a17
NPaulGuy <nikitapolgui@mail.ru>
2022-04-07 15:29:29 +0300

    OOP_Lesson44: added PHPDoc, removed unnecessary comments

--- OOP_ComposerLessons/Lesson44/src/File.php
+++ OOP_ComposerLessons/Lesson44/src/File.php
@@ -1,67 +1,122 @@
 <?php
 namespace Lesson44;
+/**
+ * [Description File]
+ */
 class File implements iFile
 {
 	private string $filePath;
+	/**
+	 * @param string $filePath
+	 */
 	public function __construct(string $filePath)
 	{
 		fopen($filePath, "w") or die("Unable to create/open file!");
 		$this->filePath = $filePath;
 	}
+	/**
+	 * @return none
+	 */
 	public function __destruct() 
 	{
 		echo "Файл $filePath удалён!<br>";
 	}
+	/**
+	 * @return string
+	 */
 	public function getPath() : string
 	{
 		return $this->filePath;
 	}
+	/**
+	 * @return string
+	 */
 	public function getDir() : string
 	{
 		return dirname($this->filePath);
 	}
+	/**
+	 * @return string
+	 */
 	public function getName() : string
 	{
 		return basename($this->filePath);
 	}
+	/**
+	 * @return string
+	 */
 	public function getExt() : string
 	{
 		return pathinfo($this->filePath, PATHINFO_EXTENSION);
 	}
+	/**
+	 * @return int
+	 */
 	public function getSize() : int
 	{
 		return strlen(file_get_contents($this->filePath));
 	}
+	/**
+	 * @return string
+	 */
 	public function getText() : string
 	{
 		return file_get_contents($this->filePath);
 	}
+	/**
+	 * @param mixed $text
+	 * 
+	 * @return iFile
+	 */
 	public function setText($text) : iFile
 	{
 		file_put_contents($this->filePath, $text);
 		return $this;
 	}
+	/**
+	 * @param mixed $text
+	 * 
+	 * @return iFile
+	 */
 	public function appendText($text) : iFile
 	{
 		file_put_contents($this->filePath, $text, FILE_APPEND);
 		return $this;
 	}
+	/**
+	 * @param mixed $copyPath
+	 * 
+	 * @return string
+	 */
 	public function copy($copyPath) : string
 	{
 		copy($this->filePath, $copyPath) or die("Cannot copy file!");
 		return file_get_contents($copyPath);
 	}
+	/**
+	 * @return none
+	 */
 	public function delete()
 	{
 		unlink($this->filePath) or die("Cannot delete file!");
 		$this->__destruct();
 	}
+	/**
+	 * @param mixed $newName
+	 * 
+	 * @return iFile
+	 */
 	public function rename($newName) : iFile
 	{
 		rename($this->filePath, $newName) or die("Cannot rename file!");
 		$this->filePath = $newName;
 		return $this;
 	}
+	/**
+	 * @param mixed $newPath
+	 * 
+	 * @return iFile
+	 */
 	public function replace($newPath) : iFile
 	{
 		rename($this->filePath, $newPath) or die("Cannot rename file!");
--- OOP_ComposerLessons/Lesson44/src/iFile.php
+++ OOP_ComposerLessons/Lesson44/src/iFile.php
@@ -3,16 +3,16 @@ namespace Lesson44;
 interface iFile
 {
 	public function __construct(string $filePath);
-	public function getPath() : string; // путь к файлу
-	public function getDir() : string;  // папка файла
-	public function getName() : string; // имя файла
-	public function getExt() : string;  // расширение файла
-	public function getSize() : int; // размер файла
-	public function getText() : string;          // получает текст файла
-	public function setText($text) : iFile;     // устанавливает текст файла
-	public function appendText($text) : iFile;  // добавляет текст в конец файла
-	public function copy($copyPath) : string;    // копирует файл
-	public function delete();           // удаляет файл
-	public function rename($newName) : iFile;   // переименовывает файл
-	public function replace($newPath) : iFile;  // перемещает файл
+	public function getPath() : string;
+	public function getDir() : string;
+	public function getName() : string;
+	public function getExt() : string;
+	public function getSize() : int;
+	public function getText() : string;
+	public function setText($text) : iFile;
+	public function appendText($text) : iFile;
+	public function copy($copyPath) : string;
+	public function delete();
+	public function rename($newName) : iFile;
+	public function replace($newPath) : iFile;
 }
