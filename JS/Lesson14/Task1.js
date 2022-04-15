34fb45780e682a7b94fe3f01553b43fd650720d7
NPaulGuy <nikitapolgui@mail.ru>
2022-04-15 15:49:12 +0300

    JS_Lesson14: added tasks

---
+++ JS/Lesson14/Task1.js
@@ -0,0 +1,6 @@
+/**
+ * Создайте скрипт, который запрашивает ввод двух чисел (используйте prompt) и после показывает их сумму.
+ */
+let num1 = +prompt("Num1 is ", 0);
+let num2 = +prompt("Num2 is ", 0);
+alert(Number(num1) + Number(num2));
